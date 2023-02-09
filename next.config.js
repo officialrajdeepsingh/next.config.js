b79b70a3ce07620cd7ab97fc40d87a2b5f6d8503
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-09 18:39:39 +0530

    adding config for next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,81 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // pageExtensions: ['mdx', 'jsx', 'js', 'tsx', 'ts'],
+  // compress: false,
+  // poweredByHeader: false,
+  // distDir: 'build',
+  // generateEtags: false,
+  // poweredByHeader: false,
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       headers: [
+  //         {
+  //           key: 'x-hello',
+  //           value: 'there',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       source: '/hello',
+  //       headers: [
+  //         {
+  //           key: 'x-hello',
+  //           value: 'world',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+       '/about': { page: '/about' },
+       '/post/hello-nextjs': { page: '/post/[slug]', query: { slug: 'hello-nextjs' } },
+       '/post/learn-nextjs': { page: '/post/[slug]', query: { slug: 'learn-nextjs' } },
+       '/post/deploy-nextjs':{ page: '/post/[slug]', query: { slug: 'deploy-nextjs' } },
+    }
+  },
+  images:{
+    unoptimized : true
+  }
+  // basePath: '/my-repo',
+//   devIndicators: {
+//     buildActivityPosition: 'bottom-right',
+//   },
+//   env: {
+//     myCustomEnvirement: 'Home pages Environment variables',
+// },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/old-rewrite-path',
+//         destination: '/new-rewrite-path',
+//       },
+//     ]
+//   },
+  
+//   async redirects() {
+//     return [
+//       {
+//         source: '/old-path-to',
+//         destination: '/new-path',
+//         permanent: true,
+//       },
+//     ]
+//   },
 }
+
 
 module.exports = nextConfig
