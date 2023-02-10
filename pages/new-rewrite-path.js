4787cc4ed8745fab35a5c0e81f482ad3b9b725bd
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-10 22:04:40 +0530

    Adding the new page

---
+++ pages/new-rewrite-path.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function newRewritePath(props) {
+    return (
+        <div>
+            <h1> new rewrite path</h1>
+        </div>
+    );
+}
+
+export default newRewritePath;
