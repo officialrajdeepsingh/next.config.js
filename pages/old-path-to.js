c9e0227ab6d92e99f58157c2e3b60f192a4f0acb
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-10 22:04:50 +0530

    Adding the new page

---
+++ pages/old-path-to.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function oldPathTo(props) {
+    return (
+        <div>
+            <h1> Old path of article</h1>
+        </div>
+    );
+}
+
+export default oldPathTo;
