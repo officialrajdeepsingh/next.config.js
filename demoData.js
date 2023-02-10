c026bd5c2a4db9a3e93c0840ef44b23a88361cdd
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-10 22:03:06 +0530

    Update demo data

--- demoData.js
+++ demoData.js
@@ -1,22 +1,108 @@
+// 7. exportPathMap
+
+// 8. URL Imports
+
+// 9. Turbopack-specific options
+
 export const demoData = [
-    {
-      "userId": 1,
-      "id": 1,
-      "slug":"hello-nextjs",
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "slug":"learn-nextjs",
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "userId": 1,
-      "id": 3,
-      "slug":"deploy-nextjs",
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    },]
+  {
+    "id":1,
+    "userId":1,
+    "property":"Environment Variables"
+  },
+  {
+    "userId": 2,
+    "id": 2,
+    "property": "Base Path"
+  },
+
+  {
+    "userId": 3,
+    "id": 3,
+    "property": "Rewrites"
+  },
+
+  {
+    "userId": 4,
+    "id": 4,
+    "property": "Redirects"
+  },
+
+  {
+    "userId": 5,
+    "id": 5,
+    "property": "Custom Headers"
+  },
+
+  {
+    "userId": 6,
+    "id": 6,
+    "property": "Custom Page Extensions"
+  },
+
+  {
+    "userId": 7,
+    "id": 7,
+    "property": "CDN Support with Asset Prefix"
+  },
+
+  {
+    "userId": 8,
+    "id": 8,
+    "property": "Custom Webpack Config"
+  },
+
+  {
+    "userId": 9,
+    "id": 9,
+    "property": "Compression"
+  },
+
+  {
+    "userId": 10,
+    "id": 10,
+    "property": " Disabling x-powered-by"
+  },
+
+  {
+    "userId": 11,
+    "id": 11,
+    "property": " React Strict Mode"
+  },
+
+  {
+    "userId": 12,
+    "id": 12,
+    "property": " Build indicator"
+  },
+
+  {
+    "userId": 13,
+    "id": 13,
+    "property": " Disabling ETag Generation"
+  },
+
+  {
+    "userId": 14,
+    "id": 14,
+    "property": " Setting a custom build directory"
+  },
+
+  {
+    "userId": 15,
+    "id": 15,
+    "property": " Trailing Slash"
+  },
+
+  {
+    "userId": 16,
+    "id": 16,
+    "property": "Ignoring ESLint"
+  },
+
+  {
+    "userId": 17,
+    "id": 17,
+    "property": "Ignoring TypeScript Errors"
+  },
+]
