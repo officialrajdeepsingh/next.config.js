ae60cc84fdfed37aae06f82c6bc3034406cbde67
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-11 15:59:01 +0530

    comment out all the config

--- next.config.js
+++ next.config.js
@@ -5,50 +5,48 @@ const nextConfig = {
   // pageExtensions: ['mdx', 'jsx', 'js', 'tsx', 'ts'],
   // compress: false,
   // poweredByHeader: false,
-
-  distDir: 'mybuild',
-
+  // distDir: 'mybuild',
   // generateEtags: false,
   // poweredByHeader: false,
-  assetPrefix: isProd ? 'https://cdn.next-config-js.vercel.app' : undefined,
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'x-index',
-            value: 'index page',
-          },
-        ],
-      },
-      {
-        source: '/about',
-        headers: [
-          {
-            key: 'x-about',
-            value: 'about us',
-          },
-        ],
-      },
-    ]
-  },
+  //  assetPrefix: isProd ? 'https://cdn.next-config-js.vercel.app' : undefined,
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       headers: [
+  //         {
+  //           key: 'x-index',
+  //           value: 'index page',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       source: '/about',
+  //       headers: [
+  //         {
+  //           key: 'x-about',
+  //           value: 'about us',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/post/hello-nextjs': { page: '/post/[slug]', query: { slug: 'hello-nextjs' } },
-      '/post/learn-nextjs': { page: '/post/[slug]', query: { slug: 'learn-nextjs' } },
-      '/post/deploy-nextjs': { page: '/post/[slug]', query: { slug: 'deploy-nextjs' } },
-    }
-  },
-  images: {
-    unoptimized: true
-  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/about': { page: '/about' },
+  //     '/post/hello-nextjs': { page: '/post/[slug]', query: { slug: 'hello-nextjs' } },
+  //     '/post/learn-nextjs': { page: '/post/[slug]', query: { slug: 'learn-nextjs' } },
+  //     '/post/deploy-nextjs': { page: '/post/[slug]', query: { slug: 'deploy-nextjs' } },
+  //   }
+  // },
+  // images: {
+  //   unoptimized: true
+  // },
   // basePath: '/my-repo',
   //   devIndicators: {
   //     buildActivityPosition: 'bottom-right',
@@ -63,24 +61,23 @@ const nextConfig = {
   //   typescript: {
   //     ignoreBuildErrors: true,
   //   },
-  async rewrites() {
-    return [
-      {
-        source: '/new-rewrite-path',
-        destination: '/old-rewrite-path',
-      },
-    ]
-  },
-
-  async redirects() {
-    return [
-      {
-        source: '/old-path-to',
-        destination: '/new-path',
-        permanent: true,
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/new-rewrite-path',
+  //       destination: '/old-rewrite-path',
+  //     },
+  //   ]
+  // },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/old-path-to',
+  //       destination: '/new-path',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
   // httpAgentOptions: {
   //   keepAlive: false,
   // },
