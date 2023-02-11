bb30ab49e14a71010278f41df592bb345b72c26b
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-11 16:00:08 +0530

    recreate the old rewrite path to explain the topic

---
+++ pages/old-rewrite-path.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function newRewritePath(props) {
+    return (
+        <div>
+            <h1> old rewrite path</h1>
+        </div>
+    );
+}
+
+export default newRewritePath;
